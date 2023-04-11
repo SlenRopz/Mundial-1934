33e2edc235bdf7f9b52a13a099c7deb9ddfff63d
Mundial1930 <marchesisanti@gmail.com>
2023-04-11 11:04:03 -0300

    Create Mundial 1934.py

---
+++ Mundial 1934.py
@@ -0,0 +1,177 @@
+1934 Mundial Italia 🇮🇹 - - - - - Campeón Italia 🇮🇹
+
+Puntos:
+🏆🥇- Italia 🇮🇹 13 pts
+🥈- Checoslovaquia 🇨🇿 9 pts
+🥉- Alemania 🇩🇪 9 pts
+⛳- Austria 🇦🇹 6 pts
+5- España 🇪🇸 4 pts
+6- Hungría 🇭🇺 3 pts
+7- Suiza 🇨🇭 3 pts
+8- Suecia 🇸🇪 3 pts
+9- Argentina 🇦🇷 0 pts
+9- Francia 🇫🇷 0 pts
+9- Países Bajos 🇳🇱 0 pts
+12- Rumania 🇷🇴 0 pts
+13- Egipto 🇪🇬 0 pts
+14- Brasil 🇧🇷 0 pts
+15- Belgica 🇧🇪 0 pts
+16- Estados Unidos 🇺🇸 0 pts
+
+
+
+
+Paises Goleadores:
+Italia 🇮🇹 12
+Alemania 🇩🇪 11
+Checoslovaquia 🇨🇿 9
+Austria 🇦🇹 7
+Hungría 🇭🇺 5
+Suiza 🇨🇭 5
+España 🇪🇸 4
+Suecia 🇸🇪 4
+Argentina 🇦🇷 2
+Francia 🇫🇷 2
+Países Bajos 🇳🇱 2
+Egipto 🇪🇬 2
+Rumania 🇷🇴 1
+Brasil 🇧🇷 1
+Estados Unidos 🇺🇸 1
+
+
+
+
+Goleadores:
+Oldrich Nejedly 🇨🇿 5
+Edmund Conen 🇩🇪 4
+Angelo Schiavio 🇮🇹 4
+Leopold Kielholz 🇨🇭 3
+Raimundo Orsi 🇮🇹 3
+
+
+
+
+Octavos de final: 41
+
+Suecia 🇸🇪 3-2 🇦🇷 Argentina
+                    4- Belis
+9 - Jonasson
+                    48- Galateo
+67- Jonasson
+79- Kroon
+
+Alemania 🇩🇪 5-2 🇧🇪 Belgica
+25- Kobierski
+                        29- Voorhoof
+                        43- Voorhoof
+49- Siffing
+66- Conen
+70- Conen
+87- Conen
+
+Suiza 🇨🇭 3-2 🇳🇱 Países Bajos
+7- Kielholz
+                   29- Smit
+43- Kielholz
+66- Abegglen
+                    69- Vente
+
+Hungría 🇭🇺 4-2 🇪🇬 Egipto
+11- Teleki
+31- Toldi
+                      35- Fawzi
+                      39- Fawzi
+53- Vincze
+61- Toldi
+
+Austria 🇦🇹 3-2 🇫🇷 Francia
+                     18- Nicolas
+44- Sindelar
+93- Schall
+109- Bican
+                      116- Verriest
+
+España 🇪🇸 3-1 🇧🇷 Brasil
+18- Iraragorri
+25- Iraragorri
+29- Langara
+                     55- Leonidas
+
+Italia 🇮🇹 7-1 🇺🇸 Estados Unidos
+18- Schiavio
+20- Orsi
+29- Schiavio
+                   57- Donelli
+63- Ferrari
+64- Schiavio
+69- Orsi
+90- Meazza
+
+
+
+
+Cuartos de final: 14
+
+Alemania 🇩🇪 2-1 🇸🇪 Suecia
+60- Hohmann
+63- Hohmann
+                         82- Dunker
+
+Checoslovaquia 🇨🇿 3-2 🇨🇭 Suiza
+                                    12- Kielholz
+24- Svoboda
+49- Sobotka
+                                     78- Jaeggi
+82- Nejedly
+
+Austria 🇦🇹 2-1 🇭🇺 Hungría
+8- Horvath
+51- Zischek
+                      60- Sarosi
+
+Italia 🇮🇹 1-1 🇪🇸 España
+                 30- Regeuiro
+44- Ferrari
+
+Italia 🇮🇹 1-0 🇪🇸 España
+11- Meazza
+
+
+
+
+Semifinales: 5
+
+Checoslovaquia 🇨🇿 3-1 🇩🇪 Alemania
+21- Nejedly
+                                    62- Noack
+69- Nejedly
+80- Nejedly
+
+Italia 🇮🇹 1-0 🇦🇹 Austria
+19- Guaita
+
+
+
+
+Tercer Puesto: 5
+
+Alemania 🇩🇪 3-2 🇦🇹 Austria
+1- Lehner
+27- Conen
+                        28- Horvath
+42- Lehner
+                        54- Sesta
+
+
+
+
+Final: 3
+
+Italia 🇮🇹 2-1 🇨🇿 Checoslovaquia
+                 71- Puc
+81- Orsi
+95- Schiavio
+
+
+
+Campeón 🏆🇮🇹 Italia
